fe698ac9f1d30bf1543c7b1ba3249f5837a3b851
Spoogius <Spoogius@gmail.com>
2022-02-03 18:04:23 -0500

    Removed debug console.log statements

--- commands/wordleScoreboard.js
+++ commands/wordleScoreboard.js
@@ -8,62 +8,7 @@ async function wordleScoreboard ( message) {
       return;
     }
  
-    //console.log( msgArr );
-    let userArray = [];
-    let msgArray = []; 
-
-    // REF: https://github.com/iColtz/discord-fetch-all/blob/main/src/functions/fetchMessages.js
-    // Reads all messages in the channel
-    let lastFetchID;
-    let keepFetching = true;
-    while( keepFetching ){
-      let fetchedMessages = await message.channel.messages.fetch({ 
-        limit:100,
-        ...( lastFetchID && { before:lastFetchID} ),
-        });
-     
-      if( fetchedMessages.size == 0 ){
-        keepFetching = false;
-      }
-      for( let [ key, value ] of fetchedMessages ){
-        if( value.content.startsWith( "Wordle" ) ){
-          userArray.push( value.author.username );
-          msgArray.push( value.content );
-        }
-      }
-      lastFetchID = fetchedMessages.lastKey( );
-    }    
-
-    // Data Structure
-    // { username : { sum : X , trys : Y } };
-    // Maybe store { username : index } so sbMap is redundent when filling avgArray
-    let sbMap = [ ];
-
-    let slashIdx = 0;
-    for( let ii = 0 ; ii < msgArray.length ; ii++ ){
-      slashIdx = msgArray[ ii ].search( "/" );
-      if( userArray[ ii ] in sbMap ){
-        sbMap[ userArray[ ii ] ][ 'sum' ] += ( msgArray[ ii ][ slashIdx - 1].charCodeAt( 0 ) ) - 48;
-        sbMap[ userArray[ ii ] ][ 'trys' ]++;
-      } else {
-        sbMap[  userArray[ ii ] ] = { 'sum' : msgArray[ ii ][ slashIdx - 1].charCodeAt( 0 )-48, 'trys' : 1 };
-      }
-    }
- 
-    avgArray = [];
-    for( let key in sbMap ){
-      avgArray.push( { [key] : { avg : sbMap[ key ][ 'sum' ]/sbMap[ key ][ 'trys' ] , trys : sbMap[ key ][ 'trys' ] } } );  
-    }
-
-    console.log("Presort: ", avgArray );
-     
-    avgArray.sort( function( a, b ){ 
-      console.log( "B: ", b );
-      let aKey = Object.keys( a );
-      let bKey = Object.keys( b );
-      console.log( a[ aKey ] );
-      console.log( b[ bKey ] );
-      if( a[ aKey ].avg > b[ bKey ].avg ){ return 1; };
+    //if( a[ aKey ].avg > b[ bKey ].avg ){ return 1; };
       if( a[ aKey ].avg < b[ bKey ].avg ){ return -1; };
       if( a[ aKey ].avg == b[ bKey ].avg ){
         if( a[ aKey ].trys > b[ bKey ].trys ){ return 1; }
@@ -76,7 +21,6 @@ async function wordleScoreboard ( message) {
     //Format Scoreboard
     let scoreboardStr = "User, Average Score, Num Wordles\n";
     for( let ii = 0 ; ii < avgArray.length ; ii++ ){
-      console.log( avgArray[ ii ] );
       let userName = Object.keys( avgArray[ ii ] );
       scoreboardStr += userName; 
       scoreboardStr += ", ";
