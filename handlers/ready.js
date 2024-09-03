1c9dabdc07f48bf19f46be27b065ec630ff36e90
Gabe Nelson <nelsong1997@gmail.com>
2024-09-02 20:30:37 -0400

    remove progress on retro role assign since you can only fetch up to 100 users who reacted

--- handlers/ready.js
+++ handlers/ready.js
@@ -3,14 +3,12 @@ const get = require('../helpers/get.js')
 const sendReminder = require('../helpers/sendReminder.js')
 
 //client
-const client = require('../client.js')
+//const client = require('../client.js')
 
 
 async function ready() {
     
     dailySetRemindTimeouts()
-
-    setRuleAgreersRole()
 
 }
 
@@ -44,43 +42,6 @@ async function dailySetRemindTimeouts() {
             setTimeout(() => sendReminder(remind.id, remindDate), tillRemind)
             // console.log(`did set timeout for reminder with id: ${remind.id}`)
         }
-    }
-}
-
-async function setRuleAgreersRole() {
-    let guildIds = client.guilds.cache.map(guild => guild.id)
-    
-    for (let guildId of guildIds) {
-        let rules = await get("rules", guildId)
-        if (!rules || !rules.channelId || !rules.rulesMsg) continue
-
-        let rulesChannel = await client.channels.fetch(rules.channelId).catch(() => {})
-        if (!rulesChannel) continue
-        let rulesMessage = await rulesChannel.messages.fetch(rules.rulesMsg).catch(() => {})
-        if (!rulesMessage) continue
-        
-        //console.log(rulesMessage.reactions.cache)
-        let trueAgreers = []
-        for (let messageReaction of rulesMessage.reactions.cache) {
-            let reaction = messageReaction[1]
-            if (reaction._emoji.name.codePointAt()!==rules.agreeEmojiId) continue
-            
-            trueAgreerUsers = await reaction.users.fetch().map(user => user.id)
-            console.log(trueAgreers)
-
-            break
-        }
-
-        break
-
-        // get list of users who reacted with the specific emoji "trueAgreers"
-        // get list of users who have the role "supposedAgreers"
-        // for each supposedAgreer...
-        //      make sure they're in trueAgreers
-        //      if not, remove the role
-        // for each trueAgreer...
-        //      check to make sure they have the role
-        //      if not, add it
     }
 }
 
